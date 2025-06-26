a48e67ca5f0bb43bce84c5c5168821ad267d002d
Weili Dai <weilidai2001@hotmail.com>
2025-06-26 23:13:41 +0100

    refactor: move mission state logic into separate slide component and data file

--- src/app/missions/[state]/page.tsx
+++ src/app/missions/[state]/page.tsx
@@ -1,71 +1,6 @@
 "use client";
 import React from "react";
-import { states } from "./states";
-import Image from "next/image";
-import { ControlsContainer } from "@/components/controls-container";
-import Button from "@/components/ui/button";
-import { JackInTheBox, Slide, Zoom } from "react-awesome-reveal";
-
-const Mission = ({ stateId }: { stateId: string }) => {
-  const state = states[stateId];
-  return (
-    <div>
-      <div className="py-[10px] mx-auto w-[310px] h-[100px] text-[18px] leading-[1.3] z-[2] flex items-center justify-center text-center">
-        {state.script && (
-          <h1
-            className="text-center"
-            dangerouslySetInnerHTML={{ __html: state.script }}
-          />
-        )}
-      </div>
-      <>
-        {state.animation === "jackInTheBox" ? (
-          <JackInTheBox key={state.heroImageUrl}>
-            <Image
-              src={state.heroImageUrl}
-              alt="hero image"
-              width={500}
-              height={500}
-            />
-          </JackInTheBox>
-        ) : state.animation === "slide" ? (
-          <Slide key={state.heroImageUrl} triggerOnce>
-            <Image
-              src={state.heroImageUrl}
-              alt="hero image"
-              width={500}
-              height={500}
-            />
-          </Slide>
-        ) : state.animation === "zoomIn" ? (
-          <Zoom key={state.heroImageUrl} triggerOnce>
-            <Image
-              src={state.heroImageUrl}
-              alt="hero image"
-              width={500}
-              height={500}
-            />
-          </Zoom>
-        ) : (
-          <Image
-            src={state.heroImageUrl}
-            alt="hero image"
-            width={500}
-            height={500}
-          />
-        )}
-      </>
-      <ControlsContainer>
-        {state.choice1 && (
-          <Button href={state.choice1.destination} text={state.choice1.text} />
-        )}
-        {state.choice2 && (
-          <Button href={state.choice2.destination} text={state.choice2.text} />
-        )}
-      </ControlsContainer>
-    </div>
-  );
-};
+import SlideComponent from "@/components/slide";
 
 export default function Page({
   params,
@@ -73,5 +8,5 @@ export default function Page({
   params: Promise<{ state: string }>;
 }) {
   const { state } = React.use(params);
-  return <Mission stateId={state} />;
+  return <SlideComponent stateId={state} />;
 }
