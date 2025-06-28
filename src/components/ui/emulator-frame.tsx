069b23b3733dd81321dba1835735b1495f11a3d9
Weili Dai <weilidai2001@hotmail.com>
2025-06-28 14:44:05 +0100

    feat: add responsive device mockup that only shows on wide screens

--- src/components/ui/emulator-frame.tsx
+++ src/components/ui/emulator-frame.tsx
@@ -1,10 +1,13 @@
+"use client";
 import { IPhoneMockup } from "react-device-mockup";
+import { useMediaQuery } from "./useMediaQuery";
 
 const EmulatorFrame = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <IPhoneMockup screenWidth={360}>{children}</IPhoneMockup>
-    </>
+  const isWide = useMediaQuery("(min-width: 500px)");
+  return isWide ? (
+    <IPhoneMockup screenWidth={360}>{children}</IPhoneMockup>
+  ) : (
+    <>{children}</>
   );
 };
 
